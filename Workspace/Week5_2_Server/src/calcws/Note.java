51d39d5d00babd563b5118b9d17bcb2c560e18f4
koen de gruijter <koen.degruijter@student.hu.nl>
2017-08-22 15:16:07 +0200

    5.2 init

---
+++ Workspace/Week5_2_Server/src/calcws/Note.java
@@ -0,0 +1,6 @@
+package calcws;
+
+public class Note {
+	public int rangnummer;
+	public String tijdstip, tekst;
+}
---
+++ Workspace/Week5_2_Server/src/calcws/NoteServer.java
@@ -0,0 +1,91 @@
+//NoteServer.java
+package calcws;
+
+import java.io.File;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.jws.WebService;
+
+@WebService
+public class NoteServer {
+
+
+	public boolean addNote(String tekst) {
+		Connection con;
+		boolean succes = false;
+		try {
+			con = getConnection();
+			Calendar c = new GregorianCalendar();
+			String query = "insert or ignore into Notes(tijdstip,tekst)\n" + "values(?, ?)";
+			PreparedStatement pstmt = con.prepareStatement(query);
+			pstmt.setString(1, "" + c.getTime());
+			pstmt.setString(2, tekst);
+			succes = pstmt.executeUpdate() != 0;
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return succes;
+	}
+
+	public boolean removeNote(int noteNr) {
+		Connection con;
+		boolean succes = false;
+		try {
+			con = getConnection();
+			PreparedStatement stmt1 = con.prepareStatement("DELETE FROM Notes WHERE rangnummer = ?;");
+			stmt1.setInt(1, noteNr);
+
+			succes = stmt1.executeUpdate() != 0;
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return succes;
+	}
+
+	public Note[] getNotes() {
+		Connection con;
+		List<Note> notes = new ArrayList<Note>();
+		try {
+			con = getConnection();
+			PreparedStatement stmt1 = con.prepareStatement("select rangnummer, tijdstip, tekst FROM Notes;");
+			stmt1.setString(1, "");
+			java.sql.ResultSet rs = stmt1.executeQuery();
+			while (rs.next()) {
+				Note note = new Note();
+				note.rangnummer = rs.getInt(1);
+				note.tijdstip = rs.getString(2);
+				note.tekst = rs.getString(3);
+				notes.add(note);
+			}
+			con.close();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		Note[] notess = new Note[notes.size()];
+		notes.toArray(notess);
+		return notess;
+	}
+
+	private Connection getConnection() throws SQLException, ClassNotFoundException {
+		// OPEN DATABASE
+		File dir = new File("database");
+		dir.mkdir();
+		Class.forName("org.sqlite.JDBC");
+		String url = "jdbc:sqlite:database/week5_2.sqlite";
+		Connection con = DriverManager.getConnection(url);
+		createTable(con);
+		return con;
+	}
+
+	private void createTable(Connection con) throws SQLException {
+		Statement stmt = con.createStatement();
+		stmt.executeUpdate("CREATE TABLE if not exists Notes (" + "rangnummer INTEGER PRIMARY KEY AUTOINCREMENT,"
+				+ "tijdstip   TEXT," + "tekst      TEXT" + ");");
+		con.close();
+}
+	
+}
